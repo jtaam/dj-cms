6fd787427cf3d6fd3469f6ef8c5d2943d8dcfc67
Tam Jid <tamjid.mdh@gmail.com>
2018-06-01 13:29:57 +0600

    post delete

--- cms/blog/urls.py
+++ cms/blog/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
 	url(r'^(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail'),
 	url(r'^category/(?P<category_slug>[-\w]+)/$', views.list_of_posts_by_category, name='list_of_posts_by_category'),
 	url(r'^(?P<slug>[-\w]+)/comment/$', views.add_comment, name='add_comment'),
+	# backend
 	url(r'^backend/post/new/$', views.new_post, name='new_post'),
 	url(r'^backend/post/$', views.list_of_posts_backend, name='list_of_posts_backend'),
 	url(r'^backend/(?P<slug>[-\w]+)/edit/$', views.edit_post, name='edit_post'),
+	url(r'^backend/(?P<slug>[-\w]+)/delete/$', views.delete_post, name='delete_post'),
 ]
